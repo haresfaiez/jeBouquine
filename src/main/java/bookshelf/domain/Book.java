b84b68245f81d11d9c9ff82089de5ffe3f81466a
Hares Faiez <faiez.hrs@gmail.com>
2015-10-24 23:03:47 +0200

    Add the book entity to the domain

---
+++ src/main/java/bookshelf/domain/Book.java
@@ -0,0 +1,60 @@
+package bookshelf.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    private String ISBN;
+    private String title;
+
+    public Book(String ISBN, String title) {
+        this.ISBN = ISBN;
+        this.title = title;
+    }
+
+    public Book() {
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (!ISBN.equals(book.ISBN)) return false;
+        if (!title.equals(book.title)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ISBN.hashCode();
+        result = 31 * result + title.hashCode();
+        return result;
+    }
+
+    public static Book createBook (String ISBN, String title) {
+        return new Book(ISBN, title);
+    }
+}
