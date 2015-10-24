c9bfff73c65f3d822ae37ebd788edf4c63bbc723
Hares Faiez <faiez.hrs@gmail.com>
2015-10-24 22:57:35 +0200

    insert cucumber test runner

---
+++ src/test/java/acceptance/book/search/RunCukesTest.java
@@ -0,0 +1,12 @@
+package acceptance.book.search;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = { "pretty", "html:target/cucumber" }
+)
+public class RunCukesTest {
+}
