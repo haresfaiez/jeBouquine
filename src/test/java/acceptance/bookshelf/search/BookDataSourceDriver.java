5be0e511b244306adc939108a93145a9a9d766f5
Hares Faiez <faiez.hrs@gmail.com>
2015-12-04 21:15:51 +0100

    Add acceptance test database initialisation
    Handle feature initial books insertion to the HSQL in-memory database.

---
+++ src/test/java/acceptance/bookshelf/search/BookDataSourceDriver.java
@@ -0,0 +1,37 @@
+package acceptance.bookshelf.search;
+
+import bookshelf.domain.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.*;
+
+public class BookDataSourceDriver {
+    public final static String PERSISTENCE_UNIT_NAME = "jbp";
+
+    private EntityManagerFactory entityManagerFactory;
+
+    public void addBooks(List<Book> books) {
+        final EntityManager entityManager =  this.entityManagerFactory
+                .createEntityManager();
+        final EntityTransaction bookInsertionTransaction = entityManager
+                .getTransaction();
+        bookInsertionTransaction.begin();
+        books.stream().forEach(book -> entityManager.persist(book));
+        bookInsertionTransaction.commit();
+        entityManager.close();
+    }
+
+    public void setUpDatabase() {
+        this.entityManagerFactory = Persistence.createEntityManagerFactory
+                (PERSISTENCE_UNIT_NAME);
+    }
+
+    public void tearDownDatabase() {
+        this.entityManagerFactory.close();
+    }
+
+
+}
--- src/test/java/acceptance/bookshelf/search/SearchForOneExistingBook.java
+++ src/test/java/acceptance/bookshelf/search/SearchForOneExistingBook.java
@@ -1,5 +1,6 @@
 package acceptance.bookshelf.search;
 
+import bookshelf.domain.Book;
 import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -7,31 +8,50 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class SearchForOneExistingBook {
-//TODO:remove those comments
+    private BookDataSourceDriver bookDataSourceDriver = new BookDataSourceDriver();
 
+    private Book expectedBook;
+    private Optional<Book> retrievedBook;
 
-    @Given("^There is a book in the catalog with ISBN \"([^\"]*)\" and title \"([^\"]*)\"$")
-    public void there_is_a_book_in_the_catalog_with_ISBN_and_title(String BookISBN, String bookTitle) throws Throwable {
-        throw new PendingException();
+    @Given("^the catalog contains these books$")
+    public void the_catalog_contains_these_books(Map<String, String> booksMap)
+            throws
+            Throwable {
+        List<Book> expectedBooksList = new ArrayList<>();
+        booksMap.forEach((isbn, title) -> expectedBooksList.add(new Book(isbn,
+                title)));
+        this.bookDataSourceDriver.addBooks(expectedBooksList);
     }
 
-    @When("^The customer search for the book by ISBN \"([^\"]*)\"$")
+    @When("^I search for a book with ISBN \"([^\"]*)\"$")
     public void the_customer_search_for_the_book_by_ISBN(String BookISBN) throws Throwable {
         throw new PendingException();
     }
 
-    @Then("^It should receive the book details$")
+    @Then("^I should get the book$")
     public void it_should_receive_the_book_details() throws Throwable {
         throw new PendingException();
     }
 
+    @Then("^I should get an missing book message$")
+    public void we_should_not_get_a_book_instance() throws Throwable {
+        throw new PendingException();
+    }
+
     @Before
-    public void setUpDatabase() {
+    public void setUp() {
+        this.bookDataSourceDriver.setUpDatabase();
     }
 
     @After
-    public void tearDownDatabase() {
+    public void tearDown() {
+        this.bookDataSourceDriver.tearDownDatabase();
     }
 
 }
