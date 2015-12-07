4052fe7be2aeb678da1d46e4f7b70975bf84d000
Hares Faiez <faiez.hrs@gmail.com>
2015-12-07 21:29:11 +0100

    Add Spring servlet initializer

---
+++ src/main/java/jebouquine/web/SpringWebInitializer.java
@@ -0,0 +1,25 @@
+package jebouquine.web;
+
+import jebouquine.SpringApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringWebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    public static final String APPLICATION_ROOT = "/";
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { SpringApplicationContext.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { SpringWebContext.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {APPLICATION_ROOT};
+    }
+}
