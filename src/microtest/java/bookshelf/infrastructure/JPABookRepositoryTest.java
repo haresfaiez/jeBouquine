623507b8a17f29b8ef70e87b274a254cf4acc212
Hares Faiez <faiez.hrs@gmail.com>
2015-12-08 22:41:10 +0100

    Remove JPABookRepository test from micro tests

---
+++ src/microtest/java/bookshelf/infrastructure/JPABookRepositoryTest.java
@@ -0,0 +1,17 @@
+package bookshelf.infrastructure;
+
+import jebouquine.domain.bookshelf.Book;
+import jebouquine.infrastructure.bookshelf.BookRepository;
+import jebouquine.infrastructure.bookshelf.JPABookRepository;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class JPABookRepositoryTest {
+
+    @Test
+    public void shouldReturnABookWhenAskForAnExistingBookByISBN () {
+
+    }
+}
