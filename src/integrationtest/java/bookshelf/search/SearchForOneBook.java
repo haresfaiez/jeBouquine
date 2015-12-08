cbd0638268624d502893a0c4a2814114a289e529
Hares Faiez <faiez.hrs@gmail.com>
2015-12-08 11:07:16 +0100

    Implement "Search for one book by isbn" integration test

--- src/integrationtest/java/bookshelf/search/SearchForOneBook.java
+++ src/integrationtest/java/bookshelf/search/SearchForOneBook.java
@@ -1,6 +1,9 @@
 package bookshelf.search;
 
+import jebouquine.service.bookshelf.BookService;
+import jebouquine.service.bookshelf.viewmodel.BookViewModel;
 import jebouquine.web.SpringWebContext;
+import jebouquine.web.bookshelf.BookSearchController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +15,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringWebContext.class})
@@ -37,14 +45,28 @@ public class SearchForOneBook {
     }
 
     @Test
-    public void shouldProvideSearchByISBN() throws Exception {
+    public void shouldProvideSearchByISBNOnHomePage() throws Exception {
         this.mockMvc.perform(get(HOME_PAGE_MAPPING))
                 .andExpect(view().name(HOME_VIEW_NAME));
     }
 
     @Test
-    public void shouldFindABookByISBN() {
-        //TODO:mock the service[use BookShelfApplicationContext]
-        //TODO:send and check a get http request
+    public void shouldReturnBookViewWhenSearchForAnExistingBookByISBN()
+            throws Exception {
+        final String ISBN = "AAAA";
+        final String title = "Hello Spring";
+        final BookViewModel expectedBookViewModel =  new BookViewModel(ISBN,
+                                                                        title);
+        BookService bookService = mock(BookService.class);
+        given(bookService.searchForBookByISBN(ISBN)).willReturn
+                (expectedBookViewModel);
+
+        BookSearchController bookSearchController = new
+                BookSearchController(bookService);
+        standaloneSetup(bookSearchController).build()
+                .perform(post("/search").param("ISBN", ISBN))
+                .andExpect(model()
+                        .attribute("book", expectedBookViewModel))
+                .andExpect(view().name("book"));
     }
 }
