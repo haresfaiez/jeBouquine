16b2edd1bae24bba40462dc1afbf521e61d7c526
Hares Faiez <faiez.hrs@gmail.com>
2015-12-08 11:11:58 +0100

    Add book view models (book search & book details)

---
+++ src/main/java/jebouquine/service/bookshelf/viewmodel/BookSearchBookViewModel.java
@@ -0,0 +1,50 @@
+package jebouquine.service.bookshelf.viewmodel;
+
+public class BookSearchBookViewModel {
+    private String ISBN;
+    private String title;
+
+    public BookSearchBookViewModel() {
+    }
+
+    public BookSearchBookViewModel(String ISBN, String title) {
+        this.ISBN = ISBN;
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BookSearchBookViewModel that = (BookSearchBookViewModel) o;
+
+        if (ISBN != null ? !ISBN.equals(that.ISBN) : that.ISBN != null)
+            return false;
+        return !(title != null ? !title.equals(that.title) : that.title != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ISBN != null ? ISBN.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
