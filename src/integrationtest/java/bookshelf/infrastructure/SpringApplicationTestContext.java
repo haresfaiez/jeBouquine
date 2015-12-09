1556d068e8c387f6fdf0ebfafeb5413d65d1e22e
Hares Faiez <faiez.hrs@gmail.com>
2015-12-09 13:56:42 +0100

    Add data source bean configuration

--- src/integrationtest/java/bookshelf/infrastructure/SpringApplicationTestContext.java
+++ src/integrationtest/java/bookshelf/infrastructure/SpringApplicationTestContext.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages = {"jebouquine"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION,
@@ -30,6 +34,11 @@ public class SpringApplicationTestContext {
         entityManager.setPersistenceUnitName(INTEGRATION_TEST_PERSISTENCE_UNIT);
         entityManager.setPackagesToScan(new String[]{"jebouquine.domain"});
         return entityManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new DriverManagerDataSource("jdbc:hsqldb:mem:jebouquine","sa","");
     }
 
     @Bean
