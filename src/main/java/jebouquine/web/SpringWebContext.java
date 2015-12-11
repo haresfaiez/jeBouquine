4a9e8ad8db1fc9ca89bdcc7e41960d09487affdf
Hares Faiez <faiez.hrs@gmail.com>
2015-12-11 11:26:39 +0100

    Add multi-language support

--- src/main/java/jebouquine/web/SpringWebContext.java
+++ src/main/java/jebouquine/web/SpringWebContext.java
@@ -3,10 +3,14 @@ package jebouquine.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -17,6 +21,9 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 //TODO:fix the scan below to focus just on jebouquine.web
 @ComponentScan("jebouquine")
 public class SpringWebContext extends WebMvcConfigurerAdapter{
+
+    public static final String LANGUAGE_URI_PARAM = "language";
+    public static final String LANG_CONFIG_LOCATION = "resources/lang/messages";
 
     @Bean
     public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
@@ -39,6 +46,34 @@ public class SpringWebContext extends WebMvcConfigurerAdapter{
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("HTML5");
         return templateResolver;
+    }
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver resolver= new SessionLocaleResolver();
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName(LANGUAGE_URI_PARAM);
+        return interceptor;
+    }
+
+    @Bean
+    public RequestMappingHandlerMapping handlerMapping
+            (LocaleChangeInterceptor localeChangeInterceptor) {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors(new Object[]{localeChangeInterceptor});
+        return handlerMapping;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(LANG_CONFIG_LOCATION);
+        return messageSource;
     }
 
     @Override
