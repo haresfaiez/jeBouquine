7a98c10dd9a915113e295b00a7387fe99889021e
Hares Faiez <faiez.hrs@gmail.com>
2015-12-13 11:43:45 +0100

    Fix book details view controller

--- src/integrationtest/java/books/web/books/ViewBookControllerTest.java
+++ src/integrationtest/java/books/web/books/ViewBookControllerTest.java
@@ -1,4 +1,42 @@
 package books.web.books;
 
+import jebouquine.service.books.BookService;
+import jebouquine.service.books.viewmodel.DetailsBookViewModel;
+import jebouquine.web.SpringWebContext;
+import jebouquine.web.books.ViewBookController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SpringWebContext.class})
+@WebAppConfiguration
 public class ViewBookControllerTest {
+
+    @Test
+    public void shouldGiveBookDetailsWhenAskedForAnExistingBookDetails() throws Exception {
+        final String bookISBN = "SPRG";
+        final String bookTitle = "Hello Spring";
+        final DetailsBookViewModel expectedBookViewModel = new
+                DetailsBookViewModel(bookISBN, bookTitle);
+        BookService bookService = mock(BookService.class);
+        when(bookService.searchForBookByISBN(bookISBN)).thenReturn
+                (expectedBookViewModel);
+        ViewBookController viewBookController = new ViewBookController
+                (bookService);
+        standaloneSetup(viewBookController).build()
+                .perform(get(String.format("/book/view/%s", bookISBN)))
+                .andExpect(view().name("book/view"))
+                .andExpect(model().attribute("bookDetails", expectedBookViewModel));
+    }
+
 }
--- src/main/java/jebouquine/web/books/ViewBookController.java
+++ src/main/java/jebouquine/web/books/ViewBookController.java
@@ -1,4 +1,29 @@
 package jebouquine.web.books;
 
+import jebouquine.service.books.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class ViewBookController {
+
+    final private BookService bookService;
+
+    @Autowired
+    public ViewBookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping(value = "/book/view/{ISBN}", method = RequestMethod.GET)
+    public String viewBookDetails(@PathVariable("ISBN") String ISBN,
+                                  Model model) {
+        model.addAttribute("bookDetails", bookService.searchForBookByISBN
+                (ISBN));
+        return "book/view";
+    }
+
 }
